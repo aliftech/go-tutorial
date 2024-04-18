7cb619d43112769a1aed49d525bd5bf52860892d
wahyukrisnaaji32 <wahyukrisnaaji32@gmail.com>
2024-04-18 13:37:57 +0700

    feat: variables

---
+++ variables/variables.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	b := "this is b"
+	fmt.Println(b)
+
+	c := true
+	fmt.Println(c)
+
+	var d int
+	fmt.Println(d)
+}
